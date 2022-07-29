322fd10c898522d5fc075ef6f875700818301ab2
Mohit Chhaparwal <mohitchhaprwal@gmail.com>
2022-07-29 23:08:38 +0530

    first try commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import Tabs from "./component/TabMenu/Tabs";
+import Todo from "./component/todoProject/todo";
+
+const App = () => {
+  return (
+    <>
+     <Todo />
+    </>
+  )
+}
+
+export default App
